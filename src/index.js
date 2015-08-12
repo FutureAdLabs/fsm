2f1095f6cacf679ba15bf4747810ac7766fc7276
Bodil Stokke <bodil@bodil.org>
2015-08-12 17:53:21 +0100

    Make it so.

---
+++ src/index.js
@@ -0,0 +1,94 @@
+/* @flow -*- mode: flow -*- */
+
+import Promise from "erx/promise";
+import * as erx from "erx";
+
+type State = string;
+type Event = string;
+type EventFn<A> = (m: Machine, data: A) => ?State;
+type StateTable = { [state: State]: { [event: Event]: EventFn } };
+type TransitionFn<A> = (m: Machine, data: A) => boolean | Promise<boolean>;
+type TransitionTable = { [state1: State]: { [state2: State]: TransitionFn } };
+type EntryFn<A> = (m: Machine, data: A) => ?State;
+type EntryTable = { [state: State]: EntryFn };
+
+function p<A>(val: A | Promise<A>): Promise<A> {
+  return (val instanceof Promise) ? val : Promise.resolved(val);
+}
+
+function match(table: TransitionTable, s1: State, s2: State): ?TransitionFn {
+  const tPrime = table[s1] || table["*"];
+  if (tPrime == null) {
+    return null;
+  }
+  return tPrime[s2] || tPrime["*"];
+}
+
+export default class Machine extends erx.Bus<State> {
+  state: State;
+  states: StateTable;
+  transitions: TransitionTable;
+  entries: EntryTable;
+  transitioning: boolean;
+
+  constructor(states: StateTable, transitions?: TransitionTable, entries?: EntryTable) {
+    super();
+    this.state = null;
+    this.states = states;
+    this.transitions = (transitions != null) ? transitions : {};
+    this.entries = (entries != null) ? entries : {};
+    this.transitioning = false;
+  }
+
+  goTo(next: State, data?: any): boolean {
+    if (this.transitioning) {
+      return false;
+    }
+    const finish = (next) => {
+      this.state = next;
+      this.push(next);
+      const eFn = this.entries[next];
+      if (eFn != null) {
+        next = eFn(this, data);
+        if (typeof next === "string") {
+          this.goTo(next, data);
+        }
+      }
+    };
+    const tFn = match(this.transitions, this.state, next);
+    if (tFn != null) {
+      this.transitioning = true;
+      p(tFn(this, data)).then((change) => {
+        this.transitioning = false;
+        if (change) {
+          finish(next);
+        }
+      });
+    } else {
+      finish(next);
+    }
+    return true;
+  }
+
+  send(event: Event, data: any): boolean {
+    if (this.transitioning) {
+      return false;
+    }
+    const prev: State = this.state;
+    const table: StateTable = this.states[this.state];
+    const fn: any = table[event]; // FIXME How to get Flow to accept this without the `any` here?
+    const next: ?State = fn ? fn(this, data) : null;
+    if (next != null) {
+      return this.goTo(next, data);
+    }
+    return true;
+  }
+
+  whileIn<A>(stream: erx.Stream<A>, state: string): erx.Stream<A> {
+    return stream.takeUntil(this.filter((s) => s !== state));
+  }
+
+  until<A>(stream: erx.Stream<A>, states: Array<string>): erx.Stream<A> {
+    return stream.takeUntil(this.filter((s) => states.indexOf(s) >= 0));
+  }
+}
---
+++ test/fsm.js
@@ -0,0 +1,85 @@
+/*global describe, it */
+
+import assert from "assert";
+import * as erx from "erx";
+
+import FSM from "../src";
+
+function asap(fn) {
+  setTimeout(fn, 1);
+}
+
+function assertSeq(c, expected, done) {
+  const acc = [];
+  c.subscribe((v) => acc.push(v), null, () => {
+    assert.deepEqual(acc, expected);
+    done();
+  });
+}
+
+function turnstile(cb) {
+  const fsm = new FSM({
+    "locked": {
+      "coin": () => "open"
+    },
+    "open": {
+      "push": () => "locked",
+      "coin": () => "open"
+    }
+  }, {
+    "locked": {
+      "open": () => cb("locked->open"),
+      "locked": () => cb("open->locked")
+    },
+    "open": {
+      "locked": () => cb("open->locked"),
+      "open": () => cb("open->open")
+    }
+  });
+  fsm.goTo("locked");
+  return fsm;
+}
+
+describe("finite state machine", () => {
+  it("the turnstile machine behaves as expected", (done) => {
+    const changes = [];
+    const m = turnstile((i) => {
+      changes.push(i);
+      return true;
+    });
+    asap(() => {
+      m.send("push");
+      asap(() => {
+        m.send("coin");
+        asap(() => {
+          m.send("coin");
+          asap(() => {
+            m.send("push");
+          });
+        });
+      });
+    });
+    assertSeq(m.take(3), ["open", "open", "locked"], () => {
+      assert.deepEqual(changes, ["locked->open", "open->open", "open->locked"]);
+      done();
+    });
+  });
+
+  it("whileIn() propagates values only until FSM state changes", (done) => {
+    const m = turnstile((i) => true);
+    const c = erx.bus();
+    setTimeout(() => c.push(1), 5);
+    setTimeout(() => { c.push(2); m.send("coin"); }, 10);
+    setTimeout(() => { c.push(3); c.close(); }, 15);
+    assertSeq(m.whileIn(c, "locked"), [1, 2], done);
+  });
+
+  it("until() propagates values only until FSM state reaches a target", (done) => {
+    const m = turnstile((i) => true);
+    const c = erx.bus();
+    setTimeout(() => c.push(1), 5);
+    setTimeout(() => { c.push(2); m.send("coin"); }, 10);
+    setTimeout(() => { c.push(3); c.close(); }, 15);
+    assertSeq(m.until(c, ["open"]), [1, 2], done);
+  });
+});
